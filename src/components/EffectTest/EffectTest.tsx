58cd67680e5261a375c734d2cc68ee2512aa2ad9
SeryiBaran <seryibaranminepy@yandex.ru>
2022-01-11 16:44:05 +0300

    Переместил комментарий в EffectTest.tsx

--- src/components/EffectTest/EffectTest.tsx
+++ src/components/EffectTest/EffectTest.tsx
@@ -4,13 +4,13 @@ import "./EffectTest.css";
 function EffectTest() {
   const [count, setCount] = useState(0);
 
-  // Я знаю, что наверняка можно было сделать проще, но я хотел хоть куда-нибудь всунуть useEffect
   let paragraphCountText = `Вы нажали на кнопочку ${count} раз`;
 
   const onButtonClick = () => {
     setCount((count) => count + 1);
   };
 
+  // Я знаю, что наверняка можно было сделать проще, но я хотел хоть куда-нибудь всунуть useEffect
   useEffect(() => {
     paragraphCountText = `Вы нажали на кнопочку ${count} раз`;
   });
