c8fe89886123c594851878a900fe9bc2729ccf92
SeryiBaran <seryibaranminepy@yandex.ru>
2022-03-12 13:34:22 +0300

    Eslint and prettier

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,4 @@ import './index.css';
 
 import App from './components/App';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root'),
-);
+ReactDOM.render(<App />, document.getElementById('root'));
