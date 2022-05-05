1153c0749cab23ca4e93dccd59cf533a77bd823a
SeryiBaran <seryibaranminepy@yandex.ru>
2022-05-05 22:21:57 +0300

    Fix

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import './index.css';
 import App from './components/App';
 
 const container = document.getElementById('root');
-const root = createRoot(container!);
+if (!container) throw new Error('Root container missing in index.html');
+const root = createRoot(container);
 
 root.render(<App />);
