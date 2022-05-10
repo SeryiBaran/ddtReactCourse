7c5f9f6a951796fc9bc7b814b2b78c58704b594d
SeryiBaran <seryibaranminepy@yandex.ru>
2022-05-10 12:30:21 +0300

    Docs in hooks

--- src/hooks/useCounter.tsx
+++ src/hooks/useCounter.tsx
@@ -6,6 +6,12 @@ interface ReturnTypes {
   decrement: () => void;
 }
 
+/**
+ *
+ * @param initialValue default 0
+ * @param step default 1
+ * @returns value, increment func and decrement func
+ */
 export const useCounter = (initialValue = 0, step = 1): ReturnTypes => {
   const [counter, setCounter] = useState<number>(initialValue);
   const increment = () => {
