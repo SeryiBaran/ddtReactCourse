6f1189ae87d68d50301e93c2312e012392a9588e
SeryiBaran <seryibaranminepy@yandex.ru>
2022-08-30 11:15:16 +0300

    Fix fonts

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -12,7 +12,5 @@ export const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     font-size: inherit;
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
   }
 `;
